c6c3e8bb9d403a1b5a3008b12e889f0740e5f3e5
ashkan semsar <ashkansemsar@hotmail.com>
2024-12-01 19:11:38 +0400

    Add project files.

---
+++ EqualityInCsharp/Program.cs
@@ -0,0 +1,22 @@
+﻿var a = new Person();
+a.Name = "Arash";
+var b = new Person();
+b.Name= "Arash";
+Console.WriteLine(a==b);
+
+Console.WriteLine(a.Equals(b));
+object c =.5;
+object d =.5;
+Console.WriteLine(c==d);
+Console.WriteLine(c.Equals(d));
+
+object x = 10;
+object z = 10;
+Console.WriteLine(x==z);
+Console.WriteLine(x.Equals(z));
+
+
+public class Person
+{
+    public string Name { get; set; }
+}
